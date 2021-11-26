3f6bbd0e90d5f433c831b025dc3cfc3665ad3e14
ADGAMERX <amank.u.m100@gmail.com>
2021-11-26 16:48:56 +0530

    added search function

---
+++ modules/scrapper.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+async function search(term) {
+
+  try {
+    var web = await axios.get(`https://www.imdb.com/find?q=${term}&s=tt&exact=true&ref_=fn_al_tt_ex`);
+  } catch {
+    return null;
+  }
+
+  const $ = cheerio.load(web.data);
+  var info = [];
+  info.title = $(
+    `#main > div > div.findSection > table > tbody > tr:nth-child(1) > td.result_text > a`
+  ).text();
+  // info.id = $(`tr.findResult:nth-child(${i}) > td:nth-child(2) > a:nth-child(1)`)[0].attribs.href.split("/")[2]
+  info.url = 'https://www.imdb.com/' + $(
+    "#main > div > div.findSection > table > tbody > tr:nth-child(1) > td.result_text > a"
+  ).attr("href");
+  console.log(info);
+}
+
+search('Satyameva Jayate');
