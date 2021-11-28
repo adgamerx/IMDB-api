4b46fd1ec7f424464336fb51d656fcbc9144ca94
ADGAMERX <amank.u.m100@gmail.com>
2021-11-28 12:38:51 +0530

    search by id function added

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const scrapper = require("./modules/namescraper");
+const searchById = require("./modules/idresp")
 
 const app = express();
 
-app.use('/api/:term', (req, res) => {
+app.use('/api/title/:term', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     let term = (req.params.term).toLowerCase();
 
     scrapper.serach(term).then((data) => {
+        if (data === null) {
+            return res.json({
+                error: 'Try different Keyword'
+            })
+
+        } else {
+            return res.send(data);
+        }
+    })
+})
+
+app.use('/api/id/:id', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    let id= (req.params.id);
+
+    searchById.searchById(id).then((data) => {
         if (data === null) {
             return res.json({
                 error: 'Try different Keyword'
